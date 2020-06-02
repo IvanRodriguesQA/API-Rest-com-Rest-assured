f9a09a9172f569dbc825a1895ab8aa6722bee774
Ivan Rodrigues <ivanrodriassis@gmail.com>
2020-06-02 16:08:45 -0300

    Integração com Junit

--- src/main/java/br/bh/ivanrodriassis/rest/OlaMundo.java
+++ src/main/java/br/bh/ivanrodriassis/rest/OlaMundo.java
@@ -3,6 +3,7 @@ package br.bh.ivanrodriassis.rest;
 import io.restassured.RestAssured;
 import io.restassured.http.Method;
 import io.restassured.response.Response;
+import io.restassured.response.ValidatableResponse;
 
 public class OlaMundo {
 
@@ -18,10 +19,13 @@ public class OlaMundo {
 //		Envio de uma requisição Get / A resposta do método colocado na variável local		
 		Response response = RestAssured.request(Method.GET, "http://restapi.wcaquino.me/ola");
 		
-//		Imprime o corpo do conteúdo da mensagem de resposta
-		System.out.println(response.getBody().asString());
+//		Imprime o corpo do conteúdo da mensagem de resposta / verificar se a resposta é Ola Mundo!
+		System.out.println(response.getBody().asString().equals("Ola Mundo!"));
 		
-//		Imprime o status code
-		System.out.println(response.statusCode());
+//		Imprime o status code / == verificar se o staus code retornado é o 200
+		System.out.println(response.statusCode() == 200);
+		
+		ValidatableResponse validacao = response.then();
+		validacao.statusCode(200);
 	}
 }
---
+++ src/main/java/br/bh/ivanrodriassis/rest/OlaMundoTest.java
@@ -0,0 +1,31 @@
+package br.bh.ivanrodriassis.rest;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.http.Method;
+import io.restassured.response.Response;
+import io.restassured.response.ValidatableResponse;
+
+public class OlaMundoTest {
+
+	@Test
+	public void testOlaMundo() {
+		
+//		Envio de uma requisição Get / A resposta do método colocado na variável local		
+		Response response = RestAssured.request(Method.GET, "http://restapi.wcaquino.me/ola");
+		
+//		Verificar se a resposta é Ola Mundo!
+		Assert.assertTrue(response.getBody().asString().equals("Ola Mundo!"));
+		
+//		Verificar se o staus code retornado é o 200
+		Assert.assertTrue(response.statusCode() == 200);
+		
+		Assert.assertTrue("O status deveria ser 200", response.statusCode() == 200);
+		Assert.assertEquals(201, response.statusCode());
+		
+		ValidatableResponse validacao = response.then();
+		validacao.statusCode(200);
+	}
+}
