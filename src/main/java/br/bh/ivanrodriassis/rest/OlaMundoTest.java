8641fac5cad2aa4ca2ecc88ee375c3b171cc03d5
Ivan Rodrigues <ivanrodriassis@gmail.com>
2020-06-03 08:59:14 -0300

    Integração com JUnit.

--- src/main/java/br/bh/ivanrodriassis/rest/OlaMundoTest.java
+++ src/main/java/br/bh/ivanrodriassis/rest/OlaMundoTest.java
@@ -1,9 +1,10 @@
 package br.bh.ivanrodriassis.rest;
 
+import static io.restassured.RestAssured.*;
+
 import org.junit.Assert;
 import org.junit.Test;
 
-import io.restassured.RestAssured;
 import io.restassured.http.Method;
 import io.restassured.response.Response;
 import io.restassured.response.ValidatableResponse;
@@ -14,7 +15,7 @@ public class OlaMundoTest {
 	public void testOlaMundo() {
 		
 //		Envio de uma requisição Get / A resposta do método colocado na variável local		
-		Response response = RestAssured.request(Method.GET, "http://restapi.wcaquino.me/ola");
+		Response response = request(Method.GET, "http://restapi.wcaquino.me/ola");
 		
 //		Verificar se a resposta é Ola Mundo!
 		Assert.assertTrue(response.getBody().asString().equals("Ola Mundo!"));
@@ -23,9 +24,31 @@ public class OlaMundoTest {
 		Assert.assertTrue(response.statusCode() == 200);
 		
 		Assert.assertTrue("O status deveria ser 200", response.statusCode() == 200);
-		Assert.assertEquals(201, response.statusCode());
+		Assert.assertEquals(200, response.statusCode());
 		
 		ValidatableResponse validacao = response.then();
 		validacao.statusCode(200);
 	}
+	
+	@Test
+	public void devoConhecerOutrosFormasRestAssured() {
+//		Envio de uma requisição Get / A resposta do método colocado na variável local		
+		Response response = request(Method.GET, "http://restapi.wcaquino.me/ola");
+		ValidatableResponse validacao = response.then();
+		validacao.statusCode(200);
+		
+		get("http://restapi.wcaquino.me/ola").then().statusCode(200);
+		
+		given() // Pré-condição -> formas de envio da requisição
+		.when() // Ação que será testada
+			.get("http://restapi.wcaquino.me/ola")
+		.then() // Verificações do resultado
+//			.assertThat();
+			.statusCode(201);
+			
+		
+	}
+	
+	
+	
 }
