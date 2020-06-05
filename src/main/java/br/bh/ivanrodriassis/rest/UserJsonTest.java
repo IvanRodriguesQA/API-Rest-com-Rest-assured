4aa135edfbd0b1aaac9f2877de2415e51968a70f
Ivan Rodrigues <ivanrodriassis@gmail.com>
2020-06-05 14:55:34 -0300

    Verificações avançadas.

--- src/main/java/br/bh/ivanrodriassis/rest/UserJsonTest.java
+++ src/main/java/br/bh/ivanrodriassis/rest/UserJsonTest.java
@@ -5,7 +5,6 @@ import static io.restassured.RestAssured.*;
 import java.util.Arrays;
 
 import org.hamcrest.Matchers;
-import org.hamcrest.Matchers.*;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -74,7 +73,7 @@ public class UserJsonTest {
 			.body("filhos", Matchers.hasSize(2)) // Verifica o tamanho da lista
 			.body("filhos[0].name", Matchers.is("Zezinho"))
 			.body("filhos[1].name", Matchers.is("Luizinho"))
-			.body("filhos.name", Matchers.hasItem("Zezinho")) // Verifica se a lista contém o elemento Zezeinho
+			.body("filhos.name", Matchers.hasItem("Zezinho")) // Verifica se a lista contém o elemento Zezinho
 			.body("filhos.name", Matchers.hasItems("Zezinho","Luizinho"))
 		;						
 	}
@@ -105,5 +104,72 @@ public class UserJsonTest {
 			.body("salary", Matchers.contains(1234.5678f, 2500, null)) // 1234.5678f -> f indica que o valor é float
 		;	
 	}
+	
+	@Test
+	public void devoFazerVerificacoesAvancadas() {
+		given()
+		.when()
+			.get("http://restapi.wcaquino.me/users")
+		.then()
+			.statusCode(200)
+			.body("$", Matchers.hasSize(3))
+			
+/*			Método que itera em cima das idades "findAll"
+            Objeto "it" instância atual da idade
+			Irá percorrer todas as idades que referencia a idade atual do objeto it  
+			Usuário com idade até 25 anos */
+			
+			.body("age.findAll{it <= 25}.size()", Matchers.is(2))  // size() verifica o tamanho da coleção
+			
+			/* Quantidade de usuários com mais de 20 anos e até 25 */
+			.body("age.findAll{it <= 25 && it > 20}.size()", Matchers.is(1))
+			
+			/* Nome do usuário com mais de 20 anos e até 25  */
+			.body("findAll{it.age <= 25 && it.age > 20}.name", Matchers.hasItem("Maria Joaquina"))  // findAll -> verifica a partir do início da raiz
+			
+			/* Retorna o primeiro usuário da lista com idade até 25 */
+			.body("findAll{it.age <= 25}[0].name", Matchers.is("Maria Joaquina"))
+			
+			/* Retorna o último usuário da lista com idade até 25 */
+			.body("findAll{it.age <= 25}[-1].name", Matchers.is("Ana Júlia")) // -1 começa a navegar do último registro da lista
+			
+			/* Retorna apena um elemento, o primeiro usuário da lista com idade até 25 */
+			.body("find{it.age <= 25}.name", Matchers.is("Maria Joaquina"))
+			
+			/* Todos os elementos cujo nome contém o "n" */
+			.body("findAll{it.name.contains('n')}.name", Matchers.hasItems("Maria Joaquina", "Ana Júlia"))
+			
+			/* Todos os elementos que possuem mais de 10 caracteres */
+			.body("findAll{it.name.length() > 10}.name", Matchers.hasItems("João da Silva", "Maria Joaquina"))
+			
+			/* Alterar as letras do nome para maiúscula e retorna o nome */
+			.body("name.collect{it.toUpperCase()}", Matchers.hasItem("MARIA JOAQUINA"))
+			
+			/* Retorna o nome de usuário que começa com Maria convertido para maiúsculo e retorna o nome encontrado */
+			.body("name.findAll{it.startsWith('Maria')}.collect{it.toUpperCase()}", 
+					Matchers.hasItem("MARIA JOAQUINA"))
+			
+			/* Retorna o nome de usuário que começa com Maria convertido para maiúsculo e retorna o nome encontrado */
+			.body("name.findAll{it.startsWith('Maria')}.collect{it.toUpperCase()}.toArray()", 
+					Matchers.allOf(Matchers.arrayContaining("MARIA JOAQUINA"), Matchers.arrayWithSize((1))))
 		
+			/* Múltiplica a idade por 2 e retorna a idade múltiplicada */
+			.body("age.collect{it*2}", Matchers.hasItems(60, 50, 40))
+			
+			/* Retorna o maior id da coleção */
+			.body("id.max()", Matchers.is(3))
+			
+			/* Retorna o menor salário da coleção */
+			.body("salary.min()", Matchers.is(1234.5678f))
+			
+			/* Retorna a soma do salário da coleção */
+			.body("salary.findAll{it != null}.sum()", Matchers.is(Matchers.closeTo(3734.5678f, 0.001)))
+			
+			/* Outra forma - Retorna a soma do salário da coleção */
+			.body("salary.findAll{it != null}.sum()", Matchers.allOf(Matchers.greaterThan(3000d),
+					Matchers.lessThan(5000d)))
+			
+		;
+		
+	}
 }
