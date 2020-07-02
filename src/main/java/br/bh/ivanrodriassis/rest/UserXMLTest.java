be755118c39725dec05720b57344132a9409f94d
Ivan Rodrigues <ivanrodriassis@gmail.com>
2020-07-02 12:50:44 -0300

    XML - nó raiz

--- src/main/java/br/bh/ivanrodriassis/rest/UserXMLTest.java
+++ src/main/java/br/bh/ivanrodriassis/rest/UserXMLTest.java
@@ -14,13 +14,23 @@ public class UserXMLTest {
 			.get("http://restapi.wcaquino.me/usersXML/3")
 		.then()
 			.statusCode(200)
-			.body("user.name", is("Ana Julia"))	
-			.body("user.@id", is("3"))
-			.body("user.filhos.name.size()", is(2))
-			.body("user.filhos.name[0]", is("Zezinho"))
-			.body("user.filhos.name[1]", is("Luizinho"))
-			.body("user.filhos.name", contains("Zezinho", "Luizinho"))
-			.body("user.filhos.name", hasItems("Zezinho", "Luizinho"))
+			
+			.rootPath("user")
+//			.body("user.name", is("Ana Julia"))	-> sem utilizar o nó raiz
+			.body("name", is("Ana Julia"))
+			.body("@id", is("3"))
+			
+			.rootPath("user.filhos")
+//			.body("filhos.name.size()", is(2)) -> Sem utilizar o nó raiz
+			.body("name.size()", is(2))
+			
+			.detachRootPath("filhos") // retira o rootPath (necessário informar filhos.)
+			.body("filhos.name[0]", is("Zezinho"))
+			.body("filhos.name[1]", is("Luizinho"))
+			
+			.appendRootPath("filhos") // adicona o rootPath (não é informado filhos.)
+			.body("name", contains("Zezinho", "Luizinho"))
+			.body("name", hasItems("Zezinho", "Luizinho"))
 		;
 	}
 }
