027239dbabbed6263d6d3ebc8b1b31addcf92e46
Ivan Rodrigues <ivanrodriassis@gmail.com>
2020-07-17 18:41:34 -0300

    Pesquisa avançada com XPath

--- src/main/java/br/bh/ivanrodriassis/rest/UserXMLTest.java
+++ src/main/java/br/bh/ivanrodriassis/rest/UserXMLTest.java
@@ -3,6 +3,7 @@ package br.bh.ivanrodriassis.rest;
 import static org.hamcrest.Matchers.*;
 
 import java.util.ArrayList;
+import java.util.concurrent.CountDownLatch;
 
 import static io.restassured.RestAssured.*;
 
@@ -75,6 +76,50 @@ public class UserXMLTest {
 		Assert.assertEquals(2, nomes.size());		
 		Assert.assertEquals("Maria Joaquina".toUpperCase(), nomes.get(0).toString().toUpperCase());
 		Assert.assertTrue("ANA JULIA".equalsIgnoreCase(nomes.get(1).toString()));
+	}
+	
+	@Test
+	public void devoFazerPesquisasAvancadasComXPath () {
+		given()
+		.when()
+			.get("http://restapi.wcaquino.me/usersXML")
+		.then()
+			.statusCode(200)
+			.body(hasXPath("count(/users/user)", is("3")))
+			.body(hasXPath("/users/user[@id='1']"))
+			.body(hasXPath("//user[@id='1']"))
+			
+			// /.. -> sobe um nível  - A partir do filho ter o nome da mãe
+			.body(hasXPath("//name[text() = 'Luizinho']/../../name", is("Ana Julia")))
+			
+			// A partir da mãe ter o nome do filho
+			.body(hasXPath("//name[text() = 'Ana Julia']/following-sibling::filhos",
+					allOf(containsString("Zezinho"), containsString("Luizinho")))) // following-sibling -> permite navegar entre os irmãos
+			
+			// Pega o nome do primeiro registro
+			.body(hasXPath("/users/user/name", is("João da Silva")))
+			.body(hasXPath("//name", is("João da Silva")))
+			
+			// Pega o nome do segundo registro
+			.body(hasXPath("/users/user[2]/name", is("Maria Joaquina")))
+			
+			//Pega a idade do terceiro registro da lista
+			.body(hasXPath("/users/user[3]/age", is("20")))	
+			
+			// Pega o último registro que existe na lista
+			.body(hasXPath("/users/user[last()]/name", is("Ana Julia")))
+			
+			// Pega pessoas que contém a letra "n" no nome
+			.body(hasXPath("count(/users/user/name[contains(., 'n')])", is("2")))
+			
+			// Pega o usuário que a idade seja menor que 24
+			.body(hasXPath("user[age < 24]/name", is("Ana Julia")))
+			
+			// Pega o usuário que tenha mais que 20 e menor que 30 anos 
+			.body(hasXPath("user[age > 20 and age < 30]/name", is("Maria Joaquina")))
+			
+			.body(hasXPath("user[age > 20][age < 30]/name", is("Maria Joaquina")))
+		;					
 	}
 	
 }
