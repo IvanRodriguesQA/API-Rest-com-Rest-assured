5e22e7aead4f5d732445dc9747dfbb0ad5105c57
Ivan Rodrigues <ivanrodriassis@gmail.com>
2020-07-23 16:10:47 -0300

    URL Paramatrizável

--- src/main/java/br/bh/ivanrodriassis/rest/VerbosTest.java
+++ src/main/java/br/bh/ivanrodriassis/rest/VerbosTest.java
@@ -81,11 +81,54 @@ public class VerbosTest {
 			.body("salary", is(1234.5678f))
 		;		
 	}
-}
-/* Formato XML do que será enviado no post
-<user> <name>Jose</name> <age>50</age> </user>
-*/
 
-// http://restapi.wcaquino.me/users/1
+	/* Formato XML do que será enviado no post
+	<user> <name>Jose</name> <age>50</age> </user>
+	*/
+
+	// http://restapi.wcaquino.me/users/1
+	
+	@Test
+	public void deveCustomizarURL() {
+		given()
+			.log().all()
+			.contentType("application/json") // (Cabeçalho) Informação que será enviado um objeto Json
+			.body("{ \"name\": \"Usuario alterado\", \"age\": 80 }") // Requisição enviando objeto Json
+		.when() 
+			// Usuário que será alterado
+			// URL Parametrizável - Com parâmetros na URL
+			.put("https://restapi.wcaquino.me/{entidade}/{userId}", "users", "1")
+		.then()		
+			.log().all()
+			.statusCode(200) // Resposta esperada - 200 (sucesso na operação)
+			.body("id", is(1))
+			.body("name", is("Usuario alterado"))
+			.body("age", is(80))
+			.body("salary", is(1234.5678f))
+		;		
+	}
+	
+	@Test
+	public void deveCustomizarURLParte2() {
+		given()
+			.log().all()
+			.contentType("application/json") // (Cabeçalho) Informação que será enviado um objeto Json
+			.body("{ \"name\": \"Usuario alterado\", \"age\": 80 }") // Requisição enviando objeto Json
+			.pathParam("entidade", "users")
+			.pathParam("userId", 1)
+		.when() 
+			// URL Parametrizável - Com parâmetros no given
+			.put("https://restapi.wcaquino.me/{entidade}/{userId}") 
+		.then()		
+			.log().all()
+			.statusCode(200) // Resposta esperada - 200 (sucesso na operação)
+			.body("id", is(1))
+			.body("name", is("Usuario alterado"))
+			.body("age", is(80))
+			.body("salary", is(1234.5678f))
+		;		
+	}
+}
+
 
 
