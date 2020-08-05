9155295956185fe971a79a81d8816a77894117b4
Ivan Rodrigues <ivanrodriassis@gmail.com>
2020-08-05 17:58:23 -0300

    Envio de dados via Header

--- src/main/java/br/bh/ivanrodriassis/rest/EnvioDadosTest.java
+++ src/main/java/br/bh/ivanrodriassis/rest/EnvioDadosTest.java
@@ -37,4 +37,20 @@ public class EnvioDadosTest {
 			.contentType(Matchers.containsString("utf-8"))
 		;		
 	}
+	
+	@Test
+	public void deveEnviarValorViaHeader() {
+		given()
+			.log().all()
+			/* accept -> quando eu quero determinar o que irá vir na resposta / 
+			 * .contentType("application/json") -> não determina o que virá na resposta */
+			.accept(ContentType.XML) 
+		.when()
+			.get("http://restapi.wcaquino.me/v2/users") // Quando não é enviado nenhum formato é retornado com um html
+		.then()
+			.log().all()
+			.statusCode(200)
+			.contentType(ContentType.XML)		
+		;		
+	}
 }
