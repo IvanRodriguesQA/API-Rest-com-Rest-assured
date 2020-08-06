6911548184de39657e7f17ceb7dc2c558c9bd8bd
Ivan Rodrigues <ivanrodriassis@gmail.com>
2020-08-06 15:13:31 -0300

    Fazer buscas com HTML

---
+++ src/main/java/br/bh/ivanrodriassis/rest/HTML.java
@@ -0,0 +1,31 @@
+package br.bh.ivanrodriassis.rest;
+
+import static org.hamcrest.Matchers.is;
+
+import org.hamcrest.Matcher;
+import org.hamcrest.Matchers;
+import org.junit.Test;
+
+import io.restassured.http.ContentType;
+
+import static io.restassured.RestAssured.given;
+
+public class HTML {
+
+	@Test
+	public void deveFazerBuscasComHTML() {
+		given()
+			.log().all()
+		.when()
+			.get("http://restapi.wcaquino.me/v2/users")
+		.then()
+			.log().all()
+			.statusCode(200)
+			.contentType(ContentType.HTML)		
+			.body("html.body.div.table.tbody.tr.size()", is(3))
+			.body("html.body.div.table.tbody.tr[1].td[2]", is("25"))
+			.appendRootPath("html.body.div.table.tbody")
+			.body("tr.find{it.toString().startsWith('2')}.td[1]", is("Maria Joaquina"))
+		;
+	}
+}
