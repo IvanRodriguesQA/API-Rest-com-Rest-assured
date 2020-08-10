1cedaee0c9a58bae0359bf53c64483184d759683
Ivan Rodrigues <ivanrodriassis@gmail.com>
2020-08-10 20:05:20 -0300

    Definindo tempo máximo dos testes

--- src/main/java/br/bh/ivanrodriassis/rest/FileTest.java
+++ src/main/java/br/bh/ivanrodriassis/rest/FileTest.java
@@ -1,7 +1,7 @@
 package br.bh.ivanrodriassis.rest;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.*;
 
 import java.io.File;
 
@@ -25,7 +25,7 @@ public class FileTest {
 	}
 	
 	@Test
-	public void deveObrigarUploadArquivo() {
+	public void deveFazerUploadArquivo() {
 		given()
 			.log().all()
 			.multiPart("arquivo", new File("src/main/resources/users.pdf"))			
@@ -35,6 +35,20 @@ public class FileTest {
 			.log().all()
 			.statusCode(200) 
 			.body("name", is("users.pdf"))			
+		;
+	}	
+	
+	@Test
+	public void naoDeveFazerUploadArquivoGrande() {
+		given()
+			.log().all()
+			.multiPart("arquivo", new File("src/main/resources/Arquivo.rar"))			
+		.when()
+			.post("http://restapi.wcaquino.me/upload")
+		.then()
+			.log().all()
+			.time(lessThan(9000L)) // Define um tempo máximo de resposta da requisição
+			.statusCode(200) 
 		;
 	}	
 	
